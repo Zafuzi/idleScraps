69902997134f2097895ace51f486159a76d66b52
Zafuzi <zacharyfoutz@gmail.com>
2023-12-02 12:06:31 -0800

    fixed bad code in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
 
 username=zafuzi
-gameName=tidepool
+gameName=idlescraps
 
-if [[ " $@ " =~ "-c " ]]; then
-	echo "removing old 'dist' ..."
-	rm -rf dist
+clean=f
+publish=f
+status=f
+zip=f
+
+while test $# != 0
+do
+    case "$1" in
+    -c) clean=t ;;
+    -p) publish=t ;;
+    -s) status=t ;;
+    -z) zip=t ;;
+    esac
+    shift
+done
+
+if [[ $clean == t ]]; then
+  echo "removing old 'dist' ..."
+  rm -rf dist
 fi
 
 echo "creating 'dist' ..."
 mkdir -p dist
 
-cp index.html dist
-cp *.js dist
-cp -rL data dist
-cp -rL components dist
+cp static/index.html dist
+cp static/*.js dist
+cp -rL static/data dist
+cp -rL static/components dist
 
+if [[ $publish == t ]]; then
+  echo "publishing to itch.io ..."
+  butler push dist $username/$gameName:html5
+fi
 
-if [[ " $@ " =~ "-z " ]]; then
+if [[ $zip == t ]]; then
   echo "creating dist.zip"
-	rm dist.zip
-	zip -r dist.zip dist;
+  rm dist.zip
+  zip -r dist.zip dist;
 fi
 
-if [[ " $@ " =~ "-p " ]]; then
-	# edit this line to push to itch
-	butler push dist $username/$gameName:html5;
+if [[ $publish == t ]]; then
+  echo "publishing to itch.io ..."
+  butler push dist.zip $username/$gameName:html5
 fi
 
-if [[ " $@ " =~ "-s " ]]; then
-	butler status $username/$gameName:html5
+if [[ $status == t ]]; then
+  echo "checking status on itch.io ..."
+  butler status $username/$gameName:html5
 fi
